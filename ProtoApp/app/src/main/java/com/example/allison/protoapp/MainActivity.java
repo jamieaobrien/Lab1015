777828f0eca522fdcb283f64e9356a85512dd6d1
AllisonBusa <allison.busa@students.olin.edu>
2018-10-31 22:06:31 -0400

    adding Android prototype app display

---
+++ ProtoApp/app/src/main/java/com/example/allison/protoapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.allison.protoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*public void BlueToothFunc(View view){
+    * Put bluetooth function here
+    * }*/
+}
