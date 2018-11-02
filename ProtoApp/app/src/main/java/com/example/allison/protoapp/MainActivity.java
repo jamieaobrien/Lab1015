50ff04a1900206409bfb4708682d9bbeddd0126d
AllisonBusa <allison.busa@students.olin.edu>
2018-11-02 11:17:44 -0400

    adding the working protoapp code for speech recognition

--- ProtoApp/app/src/main/java/com/example/allison/protoapp/MainActivity.java
+++ ProtoApp/app/src/main/java/com/example/allison/protoapp/MainActivity.java
@@ -5,16 +5,15 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.Locale;
 
-import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
-import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
+
+import static com.example.allison.protoapp.R.id.txtSpeechInput;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtSpeechInput;
@@ -26,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtSpeechInput = findViewById(R.id.textView);
+        txtSpeechInput = findViewById(R.id.txtSpeechInput);
         btnSpeak = findViewById(R.id.btnSpeak);
 
-        // hide the action bar
-        getActionBar().hide();
+//        // hide the action bar
+//        getActionBar().hide();
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
