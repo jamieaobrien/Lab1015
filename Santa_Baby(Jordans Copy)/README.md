cc68f57c930127e137cebf84ae1019094a5ba573
Jordan Crawford-O'Banner <jobanner@olin.edu>
2018-11-02 11:20:27 -0400

    Made a copy of the app for myself

---
+++ Santa_Baby(Jordans Copy)/README.md
@@ -0,0 +1,5 @@
+Hi!
+
+This is the template for the prototype of the app, which Jordan and Anya will use to add bluetooth functionality. I included the text, the button and in the places where the function should be written and called, I added comments.
+
+
---
+++ Santa_Baby(Jordans Copy)/app/src/main/java/com/example/allison/protoapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.allison.protoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static com.example.allison.protoapp.R.id.txtSpeechInput;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtSpeechInput;
+    private Button btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtSpeechInput = findViewById(R.id.txtSpeechInput);
+        btnSpeak = findViewById(R.id.btnSpeak);
+
+//        // hide the action bar
+//        getActionBar().hide();
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+
+    }
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        /* Takes user's speech input and returns it to same activity*/
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        /* Considers input in free form English*/
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        /* Extra prompt is a text prompt to show user when asking them to speak*/
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+}
