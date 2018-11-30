038f570a18c84f26ad5352b4ebe7338ec04bc4d0
AnyaCakes <anyaljensen@gmail.com>
2018-11-29 23:40:41 -0500

    Add sprint 3 to website

--- PoeIoT_MotorShield/PoeIoT_MotorShield.ino
+++ PoeIoT_MotorShield/PoeIoT_MotorShield.ino
@@ -12,32 +12,38 @@ Adafruit_DCMotor *myMotorL = AFMS.getMotor(3); //creating the object for Motor 1
 Adafruit_DCMotor *myMotorR = AFMS.getMotor(4); //creating the object for Motor 2
 
 String voice; //The input from the serial is the speech, it's stored as a string
-
+int dance_count = 0;
+int ram_count = 0;
 
 // ** Functions to Turn the Motors on and off ***
 void LeftOn(){
-   myMotorR->setSpeed(0); 
-   myMotorR->run(BACKWARD);
+   
    myMotorL->setSpeed(100); 
    myMotorL->run(BACKWARD);
 }
-
 void RightOn(){
-   myMotorL->setSpeed(0);
-   myMotorL->run(BACKWARD);
+
    myMotorR->setSpeed(100); 
    myMotorR->run(BACKWARD);
     
 }
-void Back(){
+void LeftBack(){  
+   myMotorL->setSpeed(100); 
+   myMotorL->run(FORWARD);
+}
+void RightBack(){
    myMotorR->setSpeed(100); 
-   myMotorL->setSpeed(100);
+   myMotorR->run(FORWARD);
+}
+void Back(){
+   myMotorR->setSpeed(75); 
+   myMotorL->setSpeed(75);
    myMotorL->run(FORWARD);
    myMotorR->run(FORWARD);
 }
 void BothOn(){
-   myMotorL->setSpeed(100);
-   myMotorR->setSpeed(100);
+   myMotorL->setSpeed(75);
+   myMotorR->setSpeed(75);
    myMotorL->run(BACKWARD);
    myMotorR->run(BACKWARD);
 }
@@ -46,6 +52,13 @@ void BothOff(){
    myMotorR->setSpeed(0); 
    myMotorL->run(FORWARD);
    myMotorR->run(FORWARD);
+}
+void RamOn(){
+   myMotorL->setSpeed(255);
+   myMotorR->setSpeed(255);
+   myMotorL->run(BACKWARD);
+   myMotorR->run(BACKWARD);
+   delay(1500);
 }
 
 void setup() {
@@ -61,21 +74,41 @@ void loop() {
     voice =Serial.readString();
   }
     if (voice.length() > 0) { //If there's something to read, do some functions
-      Serial.println(voice);  
+        Serial.println(voice);  
         if (voice == "left" || voice =="turn left"){
           RightOn();
+          LeftBack();
         }
         if (voice == "right" || voice =="turn right"){
           LeftOn();
+          RightBack();
         }
         if (voice == "forward" || voice=="go"){
           BothOn();
         }
         if (voice == "backward" || voice == "back"){
-
           Back();
         }
         if (voice == "stop" || voice=="off" || voice=="all off"){
+          BothOff();
+        }
+        if (voice == "dance"){
+          if (dance_count < 200) {
+            dance_count++;
+            LeftOn();
+            RightBack();
+          }
+          else if (dance_count < 400 && dance_count >= 200) {
+            dance_count++;
+            LeftBack();
+            RightOn();
+          }
+          else if(dance_count >= 1000){
+            dance_count = 0;
+          }
+        }
+        if (voice == "Ram" || voice == "ram") {
+          RamOn();
           BothOff();
         }
 voice="";
