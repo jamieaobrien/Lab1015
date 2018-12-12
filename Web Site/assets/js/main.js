1444ddf4bb1a35f4a46054f2366b9af6caf578b5
epan547 <epan547@gmail.com>
2018-12-12 18:02:47 -0500

    Home Page fixes

--- Web Site/assets/js/main.js
+++ Web Site/assets/js/main.js
@@ -24,6 +24,7 @@
 			}, 100);
 		});
 
+
 	// Dropdowns.
 		$('#nav > ul').dropotron({
 			mode: 'fade',
@@ -31,7 +32,53 @@
 			alignment: 'center'
 		});
 
-	// Nav.
+
+
+	// Navbar Collapsing
+
+	// Initial scroll position
+var scrollState = 0;
+
+// Store navbar classes
+var navClasses = document.getElementById('navBar').classList;
+
+// returns current scroll position
+var scrollTop = function() {
+  return window.scrollY;
+};
+
+// Primary scroll event function
+var scrollDetect = function(home, down, up) {
+  // Current scroll position
+  var currentScroll = scrollTop();
+  if (scrollTop() === 0) {
+    home();
+  } else if (currentScroll > scrollState) {
+    down();
+  } else {
+    up();
+  }
+  // Set previous scroll position
+  scrollState = scrollTop();
+};
+
+function homeAction() {
+  console.log("home");
+}
+
+function downAction() {
+  navClasses.remove('open');
+  navClasses.add('collapse');
+}
+
+function upAction() {
+  navClasses.remove('collapse');
+  navClasses.add('open');
+}
+
+window.addEventListener("scroll", function() {
+  scrollDetect(homeAction, downAction, upAction);
+});
 
 		// Title Bar.
 			$(
@@ -60,6 +107,8 @@
 					target: $body,
 					visibleClass: 'navPanel-visible'
 				});
+
+
 // for pictures in team
 var slideIndex = 0;
 carousel();
